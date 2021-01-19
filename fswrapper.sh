bb80877bebba133b37dadbc7ca831c111f1d1008
David Sharnoff <muir@idiom.org>
2021-01-18 21:31:42 -0800

    debugging fswrapper

--- fswrapper.sh
+++ fswrapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-fs=/`basename $0`
+fs=`basename $0`
 bin=`dirname $0`
 cmd="$1"
 
-export F="$fs"
+export F="/$fs"
 resource=`perl -n -e 'm,^/dev/drbd(\d+)\s+\Q$ENV{F}\E\s, && print "r$1\n"' /etc/fstab`
 if ! [[ "$resource" =~ ^r[0-9]+$ ]]; then
        	echo "could not determine drbd resource name" 
@@ -15,23 +15,30 @@ unset F
 shift 1 
 case "$cmd" in
 	lxc|lxd)
-		export LXD_DIR="$fs/lxd"
+		export LXD_DIR="/$fs/lxd"
 		exec $bin/$cmd "$@"
 		;;
 	start)
+		set -e
 		drbdadm primary $resource
-		mount "/$fs" && systemctl start "$fs"lxd
-		mount "/$fs/pools" && systemctl start "$fs"lxd
+		mount "/$fs" 
+		mount "/$fs/pools" 
+		sleep 1
+		systemctl start "$fs"lxd
 		;;
 	stop)
-		export LXD_DIR="$fs/lxd"
+		set -e
+		export LXD_DIR="/$fs/lxd"
 		$bin/lxc stop --all
 		$bin/lxc stop -f --all
 		systemctl stop "$fs"lxd 
 		fuser -k -m "/$fs/pools"
-		unmount "/$fs/pools"
+		umount "/$fs/pools"
+		fuser -k -m "/$fs/lxd/storage-pools/r0"
+		umount "/$fs/lxd/storage-pools/r0"
 		fuser -k -m "/$fs"
-		unmount "/$fs"
+		umount "/$fs"
+		systemctl stop multipathd
 		drbdadm secondary $resource
 		# kill -USR1 `cat /proc/pid....`
 		;;
